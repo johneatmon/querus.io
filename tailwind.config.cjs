db70f9d2992f599fa7b4e45c8c866a5e2f8817fd
johnnytest4real <jmaeatmon@icloud.com>
2022-06-06 16:50:06 -0700

    config: override default tw colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
+const colors = require("tailwindcss/colors");
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
 	content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
 	theme: {
+		colors: {
+			inherit: "inherit",
+			transparent: "transparent",
+			current: "currentColor",
+			black: colors.black,
+			white: colors.white,
+			gray: colors.gray.neutral,
+			background: "#fefafb",
+			copy: "#3b586a",
+			primary: "#5b826d",
+		},
 		extend: {
 			fontFamily: {
 				serif: ["Calendas Plus", ...defaultTheme.fontFamily.serif],
